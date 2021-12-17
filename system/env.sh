e5cb857924b6c49cd7ab165e700286dbff12af0a
Daylin Morgan <daylinmorgan@gmail.com>
2021-12-17 01:37:48 -0600

    switch to fnm based node management

--- system/env.sh
+++ system/env.sh
@@ -50,9 +50,12 @@ fi
 # ----------------
 
 # nvm settings
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+#export NVM_DIR="$HOME/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+if is-executable fnm; then
+    eval "$(fnm env)"
+fi
 
 # enhancd
 export ENHANCD_DOT_ARG="up"
