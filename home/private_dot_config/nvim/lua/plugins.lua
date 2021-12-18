e200650662a179ea0eb4e26c3a2f98e956ef0208
Daylin Morgan <daylinmorgan@gmail.com>
2021-12-17 20:37:20 -0600

    adjust alacritty and nvim configs

--- home/private_dot_config/nvim/lua/plugins.lua
+++ home/private_dot_config/nvim/lua/plugins.lua
@@ -4,7 +4,7 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  Packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  local Packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 --
 
