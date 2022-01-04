8b71deee751855ea5bc4ee87f93b03a432860636
Daylin Morgan <daylinmorgan@gmail.com>
2022-01-04 13:45:12 -0600

    update tools installer for eget v1.0.0

--- tools.sh
+++ tools.sh
@@ -3,6 +3,8 @@
 BIN_DIR=$HOME/bin
 mkdir -p $BIN_DIR
 EGET_BIN=$BIN_DIR
+YAMLDOC="info/tools.yml"
+alias eget="eget --system linux/amd64"
 
 post_download_install() {
 
@@ -13,6 +15,36 @@ post_download_install() {
     rm $temp_file
 
 }
+
+eget_tool() {
+
+    tool=$1
+
+    echo -n $tool
+
+    user=$(key=$tool yq e 'explode(.) | .[env(key)].user' $YAMLDOC)
+    asset=$(key=$tool yq e 'explode(.) | .[env(key)].asset // ""' $YAMLDOC)
+    file=$(key=$tool yq e 'explode(.) | .[env(key)].file // ""' $YAMLDOC)
+    to=$(key=$tool yq e 'explode(.) | .[env(key)].to // ""' $YAMLDOC)
+    download_only=$(key=$tool yq e 'explode(.) | .[env(key)].download-only // ""' $YAMLDOC)
+
+    eget $user/$tool \
+        ${asset:+--asset $asset} \
+        ${file:+--file $file} \
+        ${to:+--to $to}\
+        ${download_only:+--download-only} \
+        -q
+
+    if [[ $download_only ]]; then
+        echo -n ' --> '
+        echo -n 'running post-download script'
+        post_download_install $tool
+    fi
+
+    echo
+}
+
+
 
 if ! $(is-executable eget); then
 	echo "I don't see eget on your path..."
@@ -33,33 +65,27 @@ if ! $(is-executable yq); then
     eget mikefarah/yq --asset yq_linux_amd64
 fi
 
-YAMLDOC="info/tools.yml"
-alias eget="eget --system linux/amd64"
+
+if ! [ -z "$1" ]; then
+    tool=$1
+
+    echo "installing $tool"
+    info=$(key=$tool yq e 'explode(.) | .[env(key)]' $YAMLDOC)
+    if [[ "$info" == "null" ]]; then
+        echo "$tool not found in $YAMLDOC"
+        echo "exiting"
+        exit 1
+    else 
+        eget_tool $tool
+    fi
+    exit
+fi
+
+echo "no tool specified installing everything"
 
 readarray tools < <(yq e 'keys | .[]' $YAMLDOC)
 
 for tool in "${tools[@]:1}"
 do
-    echo -n $tool
-
-    user=$(key=$tool yq e 'explode(.) | .[env(key)].user' $YAMLDOC)
-    asset=$(key=$tool yq e 'explode(.) | .[env(key)].asset // ""' $YAMLDOC)
-    file=$(key=$tool yq e 'explode(.) | .[env(key)].file // ""' $YAMLDOC)
-    to=$(key=$tool yq e 'explode(.) | .[env(key)].to // ""' $YAMLDOC)
-    download_only=$(key=$tool yq e 'explode(.) | .[env(key)].download-only // ""' $YAMLDOC)
-
-    eget $user/$tool \
-        ${asset:+--asset $asset} \
-        ${file:+--file $file} \
-        ${to:+--to $to}\
-        ${download_only:+--download-only} \
-        -q
-
-    if [[ $download_only ]]; then
-        echo -n '-->'
-        echo -n 'running post-download script'
-        post_download_install $tool
-    fi
-
-    echo
+    eget_tool $tool    
 done
