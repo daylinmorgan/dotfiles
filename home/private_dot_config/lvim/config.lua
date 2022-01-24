d5bfe7f1520fbc47ab023b8a1fe80e4d0ec7e394
Daylin Morgan <daylinmorgan@gmail.com>
2022-01-24 00:03:41 -0600

    add starting lvim conifgs

--- home/private_dot_config/lvim/config.lua
+++ home/private_dot_config/lvim/config.lua
@@ -1,37 +1,39 @@
 require("keybindings")
+require("plugins")
 
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.colorscheme = "dracula"
 
-lvim.leader = "space"
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.dashboard.active = true
+lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
 
+-- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
   "bash",
+  "javascript",
   "json",
   "lua",
   "python",
+  "typescript",
   "css",
   "rust",
   "yaml",
+  "go"
 }
 
+lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
-
-lvim.plugins = {
-  {"dracula/vim",
-  as = "dracula"},
-  {"snakemake/snakemake",
-  rtp="misc/vim",
-  ft = {"Snakefile","snk"}
-  }
-}
 
 -- settings
 local opt = vim.opt
 opt.timeoutlen = 500
+-- colorscheme
+lvim.builtin.lualine.options.theme = "dracula"
+lvim.colorscheme = "dracula"
+
+
--- home/private_dot_config/lvim/lua/keybindings.lua
+++ home/private_dot_config/lvim/lua/keybindings.lua
@@ -6,8 +6,10 @@ function map(mode, keys, action, options)
   vim.api.nvim_set_keymap(mode,keys,action,options)
 end
 
-
-
-
 map('i','jk','<Esc>',{noremap=true})
 map('v','jk','<Esc>',{noremap=true})
+
+-- keymappings [view all the defaults by pressing <leader>Lk]
+-- lvim.leader = "space"
+-- add your own keymapping
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
