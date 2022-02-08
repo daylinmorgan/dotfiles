a4315311990e75e7f1dba48d1d27233a5b3a14b1
Daylin Morgan <daylinmorgan@gmail.com>
2022-02-08 10:06:36 -0600

    update alacritty and lvim timeout

--- home/private_dot_config/lvim/config.lua
+++ home/private_dot_config/lvim/config.lua
@@ -35,7 +35,7 @@ lvim.colorscheme = "dracula"
 
 -- settings
 local opt = vim.opt
-opt.timeoutlen = 500
+opt.timeoutlen = 200
 lvim.autocommands.custom_groups = {
   -- On entering insert mode in any file, scroll the window so the cursor line is centered
   {"InsertEnter", "*", ":normal zz"},
