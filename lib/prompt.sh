2638a852b33cf15d9e17559b298c816a803645aa
Daylin Morgan <daylinmorgan@gmail.com>
2022-07-31 16:40:45 +0000

    make prompt/extensions tool installed dependent

--- lib/prompt.sh
+++ lib/prompt.sh
@@ -1,8 +1,12 @@
 autoload -Uz compinit
 compinit
 
-eval "$(sheldon -q --config-file ~/.config/sheldon/plugins.toml source)"
+if is-executable sheldon; then
+	eval "$(sheldon -q --config-file ~/.config/sheldon/plugins.toml source)"
+fi
+
+# TODO: make decent fall back shell
+
+is-executable starship && eval "$(starship init zsh)"
 
 is-executable zoxide && eval eval "$(zoxide init zsh --cmd cd)"
-
-eval "$(starship init zsh)"
