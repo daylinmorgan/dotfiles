28ff4f60e9d9dd0cb596f6cbea348f0b670a2355
Daylin Morgan <daylinmorgan@gmail.com>
2022-08-01 21:34:07 +0000

    fix formatting in prompt

--- lib/prompt.sh
+++ lib/prompt.sh
@@ -1,8 +1,12 @@
+#!/usr/bin/env zsh
+
 autoload -Uz compinit
 compinit
 
 if is-executable sheldon; then
 	eval "$(sheldon -q --config-file ~/.config/sheldon/plugins.toml source)"
+else
+    echo "no sheldon = no shell extensions loaded"
 fi
 
 is-executable zoxide && eval eval "$(zoxide init zsh --cmd cd)"
@@ -10,8 +14,12 @@ is-executable zoxide && eval eval "$(zoxide init zsh --cmd cd)"
 if is-executable starship; then
 	eval "$(starship init zsh)"
 else
+	echo "no starship = dumber prompt"
+
 	autoload -Uz vcs_info
-	precmd() { vcs_info }
+	precmd() {
+		vcs_info
+	}
 
 	zstyle ':vcs_info:git:*' formats '%b '
 
