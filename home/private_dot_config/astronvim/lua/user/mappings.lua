747ca94eaf40904cbce0e031c92c555e8e42c8be
Daylin Morgan <daylinmorgan@gmail.com>
2022-11-11 21:41:26 -0600

    add initial astronvim config

---
+++ home/private_dot_config/astronvim/lua/user/mappings.lua
@@ -0,0 +1,21 @@
+-- Mapping data with "desc" stored directly by vim.keymap.set().
+--
+-- Please use this mappings table to set keyboard mapping since this is the
+-- lower level configuration and more robust one. (which-key will
+-- automatically pick-up stored data by this setting.)
+return {
+  -- first key is the mode
+  n = {
+    -- second key is the lefthand side of the map
+    -- mappings seen under group name "Buffer"
+    ["<leader>bb"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+    ["<leader>bc"] = { "<cmd>BufferLinePickClose<cr>", desc = "Pick to close" },
+    ["<leader>bj"] = { "<cmd>BufferLinePick<cr>", desc = "Pick to jump" },
+    ["<leader>bt"] = { "<cmd>BufferLineSortByTabs<cr>", desc = "Sort by tabs" },
+    ["<leader>bp"] = { "<cmd>:bp<cr>", desc = "Previous Buffer" },
+  },
+  t = {
+    -- setting a mapping to false will disable it
+    -- ["<esc>"] = false,
+  },
+}
---
+++ home/private_dot_config/astronvim/lua/user/plugins/init.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "quarto-dev/quarto-vim",
+    requires = {
+      { "vim-pandoc/vim-pandoc-syntax" },
+    },
+    ft = { "quarto" },
+  },
+  { 'xiyaowong/nvim-transparent',
+    config = function()
+      -- todo: move to separate file
+      require("transparent").setup({
+        enable = true, -- boolean: enable transparent
+        extra_groups = { -- table/string: additional groups that should be cleared
+          -- In particular, when you set it to 'all', that means all available groups
+          -- example of akinsho/nvim-bufferline.lua
+          "BufferLineTabClose",
+          "BufferlineBufferSelected",
+          "BufferLineFill",
+          "BufferLineBackground",
+          "BufferLineSeparator",
+          "BufferLineIndicatorSelected",
+        },
+        exclude = {}, -- table: groups you don't want to clear
+      })
+    end
+  },
+  { "catppuccin/nvim", as = "catppuccin",
+    config = function()
+      require("catppuccin").setup {}
+    end,
+  },
+}
