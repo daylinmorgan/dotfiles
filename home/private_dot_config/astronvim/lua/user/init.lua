08c9994d888c5d5fdf84d17acfdad7fd0e230fa6
Daylin Morgan <daylinmorgan@gmail.com>
2022-11-14 11:48:35 -0600

    add nix/and fix nvim notify

--- home/private_dot_config/astronvim/lua/user/init.lua
+++ home/private_dot_config/astronvim/lua/user/init.lua
@@ -4,8 +4,10 @@ local config = {
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
-  --polish = function()
-  --end,
+  polish = function()
+    -- todo: convert to lua
+    vim.cmd('cnoremap sudow w !sudo tee % >/dev/null')
+  end,
 }
 
 return config
---
+++ home/private_dot_config/astronvim/lua/user/plugins/notify.lua
@@ -0,0 +1,5 @@
+return function(config)
+  -- for compatability with nvim-transparent
+  config.background_color = "#000000"
+  return config
+end
