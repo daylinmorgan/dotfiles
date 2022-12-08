9e563c1d7f62d970af63f58b48527efe2c8dd1ee
Daylin Morgan <daylinmorgan@gmail.com>
2022-12-08 11:01:14 -0600

    change umamba startup

--- lib/conda.zsh
+++ lib/conda.zsh
@@ -18,25 +18,29 @@ unset __conda_setup
 if [ -f "$HOME/mambaforge/etc/profile.d/mamba.sh" ]; then
 	. "$HOME/mambaforge/etc/profile.d/mamba.sh"
 fi
+
 # <<< conda initialize <<<
 
 # ! is-tty && [ -d "$HOME/mambaforge/envs/dev" ] && conda activate dev
 
 # >>> mamba initialize >>>
 # !! Contents within this block are managed by 'mamba init' !!
-export MAMBA_EXE="$HOME/.local/bin/micromamba"
-export MAMBA_ROOT_PREFIX="$HOME/micromamba"
-__mamba_setup="$("$HOME/.local/bin/micromamba" shell hook --shell zsh --prefix "$HOME/micromamba" 2>/dev/null)"
+export MAMBA_EXE="$HOME/bin/micromamba";
+export MAMBA_ROOT_PREFIX="$HOME/micromamba";
+__mamba_setup="$('$HOME/bin/micromamba' shell hook --shell zsh --prefix '$HOME/micromamba' 2> /dev/null)"
 if [ $? -eq 0 ]; then
-	eval "$__mamba_setup"
+    eval "$__mamba_setup"
 else
-	if [ -f "$HOME/micromamba/etc/profile.d/micromamba.sh" ]; then
-		. "$HOME/micromamba/etc/profile.d/micromamba.sh"
-	else
-		export PATH="$HOME/micromamba/bin:$PATH" # extra space after export prevents interference from conda init
-	fi
+    if [ -f "$HOME/micromamba/etc/profile.d/micromamba.sh" ]; then
+        . "$HOME/micromamba/etc/profile.d/micromamba.sh"
+    else
+        export  PATH="$HOME/micromamba/bin:$PATH"  # extra space after export prevents interference from conda init
+    fi
 fi
 unset __mamba_setup
+# <<< mamba initialize <<<
+
+
 # <<< mamba initialize <<<
 
 snake() {
