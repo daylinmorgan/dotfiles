04ae830fd1c2f3496bd1e5489f26e4f5e0931af6
Daylin Morgan <daylinmorgan@gmail.com>
2022-12-13 13:18:51 -0600

    docker image broken till PR merged in eget

--- docker/install-tools.sh
+++ docker/install-tools.sh
@@ -6,18 +6,20 @@
 #
 export PATH="$PATH:/home/daylin/bin:/home/daylin/.dotfiles/bin"
 export EGET_BIN=/home/$USER/bin
+export EGET_CONFIG=/home/$USER/.config/eget/eget.toml
 export DOTFILES_DIR=/home/$USER/.dotfiles
 
 curl https://zyedidia.github.io/eget.sh | sh && mv ./eget ./bin/eget
+eget Tomwright/dasel --asset dasel_linux_amd64
 
-tools $(cat "$DOTFILES_DIR/docker/tools.txt")
+aweget $(awk '{printf "-i %s ", $0}' "$DOTFILES_DIR/docker/tools.txt")
 
 # eget rossmacarthur/sheldon
 eget tree-sitter/tree-sitter
 
 # installing nvim
 eget neovim/neovim --to ./nvim.appimage --pre-release
-./nvim.appimage --appimage-extract
+./nvim.appimage --appimage-extract >/dev/null 2>&1
 ./squashfs-root/AppRun --version
 
 mv squashfs-root /
