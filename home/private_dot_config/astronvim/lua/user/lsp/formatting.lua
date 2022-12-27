7ce8d147c3b544a1246640b61d410b95fc7cfc0e
Daylin Morgan <daylinmorgan@gmail.com>
2022-12-27 16:04:55 -0600

    add formatting to chezmoi

---
+++ home/private_dot_config/astronvim/lua/user/lsp/formatting.lua
@@ -0,0 +1,9 @@
+return {
+    format_on_save = {
+        enabled = true, -- enable format on save
+        ignore_filetypes = { -- disable format on save for specified filetypes
+            "markdown",
+            "python",
+        },
+    },
+}
