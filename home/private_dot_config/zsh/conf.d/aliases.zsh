517136656a2749520ec7f11b71543e308b18139a
Daylin Morgan <daylinmorgan@gmail.com>
2023-02-20 15:18:18 -0600

    tmux configs somewhere else already

--- home/private_dot_config/zsh/conf.d/aliases.zsh
+++ home/private_dot_config/zsh/conf.d/aliases.zsh
@@ -36,12 +36,3 @@ alias zshrc='${EDITOR:-vim} "${ZDOTDIR:-$HOME}"/.zshrc'
 # alias zbench='for i in {1..10}; do /usr/bin/time zsh -lic exit; done'
 alias zdot='cd ${ZDOTDIR:-~}'
 
-# tmux
-alias t-labbook="$DOTFILES_DIR/tmux/labbook.sh"
-# source custom tmux.conf with older tmux
-alias tmux="tmux -f ~/.config/tmux/tmux.conf"
-alias t='tmux'
-alias tn='tmux new-session'
-alias tl='tmux list-sessions'
-
-
