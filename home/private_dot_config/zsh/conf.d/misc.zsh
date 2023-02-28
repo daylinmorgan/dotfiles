f094cdec07fe7c4810258b0a45a4907722e62145
Daylin Morgan <daylinmorgan@gmail.com>
2023-02-28 12:10:59 -0600

    many shell things

--- home/private_dot_config/zsh/conf.d/misc.zsh
+++ home/private_dot_config/zsh/conf.d/misc.zsh
@@ -2,24 +2,17 @@
 # misc - Set general Zsh config options here, or change plugin settings.
 #
 
-#
 # Options
-#
-
 # Undo options from plugins
 setopt NO_BEEP       # Be quiet!
 setopt NO_HIST_BEEP  # Be quiet!
 
-#
-# OMZ
-#
-MAGIC_ENTER_GIT_COMMAND="$MAGIC_ENTER_OTHER_COMMAND && git status -sb"
+# start gpg agent
+export GPG_TTY=$(tty)
+gpgconf --launch gpg-agent
 
-#
-# Zsh-Utils
-#
-# The belek/zsh-utils completion plugin also introduces compstyles. Let's use that!
-(( ! $+functions[compstyle_zshzoo_setup] )) || compstyle_zshzoo_setup
+# OMZ
+MAGIC_ENTER_GIT_COMMAND="$MAGIC_ENTER_OTHER_COMMAND && git status -sb"
 
 # let make handle it's own shell completion
 zstyle ':completion::complete:make:*:targets' call-command true
--- home/private_dot_config/zsh/zexists.d/cmd/lsd.zsh
+++ home/private_dot_config/zsh/zexists.d/cmd/lsd.zsh
@@ -1,3 +1,3 @@
 is-tty && alias lsd="lsd --icon never"
-alias ls="lsd --group-directories-first --color=always"
+alias ls="lsd"
 alias lt='lsd --tree --depth=3'
