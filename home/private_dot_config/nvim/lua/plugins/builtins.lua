2b5e645faa0782dc0e427a7fcdc410373f6250ef
Daylin Morgan <daylinmorgan@gmail.com>
2023-03-20 09:13:07 -0500

    updates to nvim

--- home/private_dot_config/nvim/lua/plugins/builtins.lua
+++ home/private_dot_config/nvim/lua/plugins/builtins.lua
@@ -45,8 +45,24 @@ return {
         "shellcheck",
         "shfmt",
         "ruff",
+        "ruff-lsp",
+        "nimlsp",
         "lua-language-server",
       },
     },
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function()
+      local nls = require("null-ls")
+      return {
+        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+        sources = {
+          nls.builtins.formatting.stylua,
+          nls.builtins.formatting.shfmt,
+          nls.builtins.diagnostics.ruff,
+        },
+      }
+    end,
   },
 }
