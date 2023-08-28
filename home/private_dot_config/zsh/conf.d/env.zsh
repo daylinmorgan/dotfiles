3db58e9f4afd46f9deb6299f661bdb08d2ce10b0
Daylin Morgan <daylinmorgan@gmail.com>
2023-08-28 14:39:48 -0500

    add env.zsh

---
+++ home/private_dot_config/zsh/conf.d/env.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+# export PYTHONPATH="$PYTHONPATH:$(viv m s -p)"
+export PYTHONPATH="$PYTHONPATH:$HOME/.local/share/viv"
+export VIV_NO_SETUPTOOLS=1
+export VIV_RUN_MODE='semi-ephemeral'
+
