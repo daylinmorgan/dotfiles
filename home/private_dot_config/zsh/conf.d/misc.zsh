74aa57d6913028678f5ed3ba6eda018b3206b6b1
Daylin Morgan <daylinmorgan@gmail.com>
2023-12-15 09:12:54 -0600

    no gpg agent launch here

--- home/private_dot_config/zsh/conf.d/misc.zsh
+++ home/private_dot_config/zsh/conf.d/misc.zsh
@@ -7,10 +7,6 @@
 setopt NO_BEEP       # Be quiet!
 setopt NO_HIST_BEEP  # Be quiet!
 
-# start gpg agent
-export GPG_TTY=$(tty)
-gpgconf --launch gpg-agent
-
 # OMZ
 MAGIC_ENTER_GIT_COMMAND="$MAGIC_ENTER_OTHER_COMMAND && git status -sb"
 
