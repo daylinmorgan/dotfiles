f4f8b76f725133e20326d24b3cfe96a651c95142
Daylin Morgan <daylinmorgan@gmail.com>
2024-01-02 08:40:01 -0600

    no more loose system volume directory

--- home/private_dot_config/zsh/zexists.d/cmd/lsd.zsh
+++ home/private_dot_config/zsh/zexists.d/cmd/lsd.zsh
@@ -1,3 +1,3 @@
 is-tty && alias lsd="lsd --icon never"
-alias ls="lsd --group-directories-first --color=auto -I 'System Volume Information' -g"
+alias ls="lsd --group-directories-first --color=auto -g"
 alias lt='lsd --tree --depth=3'
