30f518235d4bd322c95be50fcf1a9da4caed4074
Daylin Morgan <daylinmorgan@gmail.com>
2024-02-26 10:11:37 -0600

    drop the emoji support for now

--- home/private_dot_config/nvim/lua/plugins/builtins.lua
+++ home/private_dot_config/nvim/lua/plugins/builtins.lua
@@ -15,15 +15,15 @@ return {
     },
   },
   -- add emoji completion
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local cmp = require("cmp")
-      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
-    end,
-  },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   dependencies = { "hrsh7th/cmp-emoji" },
+  --   ---@param opts cmp.ConfigSchema
+  --   opts = function(_, opts)
+  --     local cmp = require("cmp")
+  --     opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
+  --   end,
+  -- },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
