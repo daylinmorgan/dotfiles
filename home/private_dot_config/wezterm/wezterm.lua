f281790b4b67ec96e20c056cab9e47561138a975
Daylin Morgan <daylinmorgan@gmail.com>
2024-03-14 09:47:25 -0500

    various hyprland/wayland things

--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require("wezterm")
 local catppuccin = require("colors/catppuccin").setup({})
-local keys = require("keys")
+-- local keys = require("keys")
 
 local padding = 10
 local config = {}
@@ -27,7 +27,11 @@ config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 --config.disable_default_key_bindings = true
+config.enable_wayland = false
 config.bold_brightens_ansi_colors = true
 config.default_gui_startup_args = { "start", "--always-new-process" }
 -- config.keys = keys.keys
+-- config.window_background_opacity = .9
+-- config.text_background_opacity = .9
+
 return config
