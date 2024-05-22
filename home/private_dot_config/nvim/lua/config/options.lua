36c47f574bf9cec642dc0a7cc60500b2269ee768
Daylin Morgan <me@dayl.in>
2024-05-22 11:48:17 -0500

    nvim be nvimming as always

--- home/private_dot_config/nvim/lua/config/options.lua
+++ home/private_dot_config/nvim/lua/config/options.lua
@@ -10,5 +10,12 @@ vim.opt.timeoutlen = 200
 
 vim.g.autoformat = false
 
+
+-- make .roc files have the correct filetype
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  pattern = { "*.roc" },
+  command = "set filetype=roc",
+})
+
 -- https://github.com/ziglang/zig.vim/issues/51#issuecomment-1637180522
 vim.g.zig_fmt_autosave = 0
