efa4e5ab4becbf36e16077788cbb75ea34880216
Daylin Morgan <me@dayl.in>
2024-05-22 11:48:25 -0500

    trying out eza...it's nice

--- home/private_dot_config/zsh/zexists.d/cmd/eza.sh
+++ home/private_dot_config/zsh/zexists.d/cmd/eza.sh
@@ -3,12 +3,13 @@
 export EZA_COLORS="da=37"
 export EZA_MIN_LUMINANCE=50
 
-alias ls='eza --icons'
-alias l='eza -lbF --git --icons'
-alias llm='eza -lbGd --git --sort=modified --icons'
-alias lx='eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale --icons'
+is-tty || alias eza="eza --icons"
+alias ls='eza'
+alias l='eza -lbF --git'
+alias llm='eza -lbGd --git --sort=modified'
+alias lx='eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale'
 
 alias lS='eza -1'
-alias lt='eza --tree --level=2 --icons'
+alias lt='eza --tree --level=2'
 alias l.="eza -a | grep -E '^\.'"
 
