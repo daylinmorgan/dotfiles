84c376524db1fafe36ae531d32d2b56c6e95daa8
Daylin Morgan <me@dayl.in>
2024-05-22 12:53:47 -0500

    fix wezterm clipboard issue

--- home/private_dot_config/wezterm/keys.lua
+++ home/private_dot_config/wezterm/keys.lua
@@ -1,22 +1,12 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 
-local M = {}
+return {
+	-- { key = "C", mods = "CTRL|SHIFT", action = act.CopyTo("ClipboardAndPrimarySelection") },
 
-M.keys = {
-	{ key = "C", mods = "CTRL|SHIFT", action = act.CopyTo("ClipboardAndPrimarySelection") },
-
-	-- paste from the clipboard
-	-- { key = "V", mods = "CTRL|SHIFT", action = act.PasteFrom("Clipboard") },
-	-- -- paste from the primary selection
-	-- { key = "V", mods = "CTRL|SHIFT", action = act.PasteFrom("PrimarySelection") },
-	--
-	-- change font size
 	{ key = "-", mods = "CTRL", action = "DecreaseFontSize" },
 	{ key = "=", mods = "CTRL", action = "IncreaseFontSize" },
 
 	-- { key = "Space", mods = "CTRL|SHIFT", action = "QuickSelect" },
 	{ key = "Space", mods = "CTRL|SHIFT", action = "ActivateCopyMode" },
 }
-
-return M
--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -1,17 +1,10 @@
 local wezterm = require("wezterm")
 local catppuccin = require("colors/catppuccin").setup({})
--- local keys = require("keys")
 
 local padding = 10
-local config = {}
 
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
+local config = wezterm.config_builder()
 
-config.hyperlink_rules = wezterm.default_hyperlink_rules()
-
--- window_background_opacity = 0.9
 config.check_for_updates = false
 config.window_padding = {
 	left = padding,
@@ -20,20 +13,22 @@ config.window_padding = {
 	bottom = padding,
 }
 
-config.hide_mouse_cursor_when_typing = false
-config.font = wezterm.font("MonoLisa Nerd Font")
-config.adjust_window_size_when_changing_font_size = false
-config.initial_cols = 80
 config.colors = catppuccin
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
---config.disable_default_key_bindings = true
-config.enable_wayland = false
-config.bold_brightens_ansi_colors = true
-config.default_gui_startup_args = { "start", "--always-new-process" }
--- config.keys = keys.keys
+config.font = wezterm.font("MonoLisa Nerd Font")
+-- config.disable_default_key_bindings = true
+-- config.bold_brightens_ansi_colors = true
 -- config.window_background_opacity = .9
 -- config.text_background_opacity = .9
+
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+config.hide_mouse_cursor_when_typing = false
+config.adjust_window_size_when_changing_font_size = false
+config.initial_cols = 80
+config.enable_wayland = false
+config.keys = require("keys")
+config.default_gui_startup_args = { "start", "--always-new-process" }
 
 return config
