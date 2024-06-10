5053ae0ab2d1c2e53d0319e2bc6e862cba303de4
Daylin Morgan <me@dayl.in>
2024-06-10 10:07:31 -0500

    remove old .task.cfg.mk

--- .task.cfg.mk
+++
@@ -1,7 +0,0 @@
-GOAL_STYLE = b_magenta
-HELP_SEP = {a.b_green}->>{a.end}
-USAGE = {a.italic}{a.b_cyan}Best Dots Around{a.end}\n
-PHONIFY = 1
-
--include .task.mk
-$(if $(wildcard .task.mk),,.task.mk: ; @curl -fsSL https://raw.githubusercontent.com/daylinmorgan/task.mk/main/task.mk -o .task.mk 2>/dev/null || echo "no curl, skipping task.mk download")
