da8259fc9e32fd7fab59f6f7cea6d3f8526498dc
Daylin Morgan <me@dayl.in>
2024-06-10 10:35:21 -0500

    test out omp

--- home/private_dot_config/zsh/plugins/prompt/prompt.plugin.zsh
+++ home/private_dot_config/zsh/plugins/prompt/prompt.plugin.zsh
@@ -3,13 +3,15 @@
 
 0=${(%):-%N}
 # fpath+="${0:A:h}/functions"
-autoload -Uz promptinit && promptinit
+# autoload -Uz promptinit && promptinit
+#
+# if zmodload zsh/terminfo && (( terminfo[colors] >= 256 )); then
+#   [[ ! -f $ZDOTDIR/themes/.p10k.zsh ]] || source $ZDOTDIR/themes/.p10k.zsh
+# else
+#   [[ ! -f $ZDOTDIR/themes/.p10k-ascii.zsh ]] || source $ZDOTDIR/themes/.p10k-ascii.zsh
+# fi
+# prompt powerlevel10k
 
-if zmodload zsh/terminfo && (( terminfo[colors] >= 256 )); then
-  [[ ! -f $ZDOTDIR/themes/.p10k.zsh ]] || source $ZDOTDIR/themes/.p10k.zsh
-else
-  [[ ! -f $ZDOTDIR/themes/.p10k-ascii.zsh ]] || source $ZDOTDIR/themes/.p10k-ascii.zsh
-fi
+eval "$(oh-my-posh init zsh --config $XDG_CONFIG_HOME/omp/config.yml)"
 
-prompt powerlevel10k
 
