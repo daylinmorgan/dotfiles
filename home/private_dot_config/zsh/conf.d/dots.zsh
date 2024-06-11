44d128df4ad0520f3df846a39accdba354380208
Daylin Morgan <me@dayl.in>
2024-06-11 11:57:45 -0500

    dots-add better and better

--- home/private_dot_config/zsh/conf.d/dots.zsh
+++ home/private_dot_config/zsh/conf.d/dots.zsh
@@ -4,7 +4,7 @@ alias dots='cd ${DOTFILES_DIR:-~/.dotfiles}'
 alias dots-drop='chezmoi forget --interactive $(chezmoi managed -p absolute | fzf -m)'
 # alias dots-add='chezmoi re-add --interactive'
 function dots-add {
-  chezmoi add $(chezmoi status | grep '^MM' | awk '{print $2}' | fzf -m)
+  chezmoi add $(chezmoi status | grep '^MM' | awk -v home="$HOME/" '{print home$2}' | fzf -m)
 }
 
 
