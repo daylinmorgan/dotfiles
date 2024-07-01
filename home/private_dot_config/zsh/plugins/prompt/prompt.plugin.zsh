01cb06f654e19aaa9666e90e410e25e676136bd5
Daylin Morgan <me@dayl.in>
2024-07-01 13:33:02 -0500

    go back to p10k

--- home/private_dot_config/zsh/plugins/prompt/prompt.plugin.zsh
+++ home/private_dot_config/zsh/plugins/prompt/prompt.plugin.zsh
@@ -1,16 +1,16 @@
 #!/usr/bin/env zsh
 
 
-0=${(%):-%N}
+# 0=${(%):-%N}
 # fpath+="${0:A:h}/functions"
-# autoload -Uz promptinit && promptinit
-#
-# if zmodload zsh/terminfo && (( terminfo[colors] >= 256 )); then
-#   [[ ! -f $ZDOTDIR/themes/.p10k.zsh ]] || source $ZDOTDIR/themes/.p10k.zsh
-# else
-#   [[ ! -f $ZDOTDIR/themes/.p10k-ascii.zsh ]] || source $ZDOTDIR/themes/.p10k-ascii.zsh
-# fi
-# prompt powerlevel10k
+autoload -Uz promptinit && promptinit
+
+if zmodload zsh/terminfo && (( terminfo[colors] >= 256 )); then
+  [[ ! -f $ZDOTDIR/themes/.p10k.zsh ]] || source $ZDOTDIR/themes/.p10k.zsh
+else
+  [[ ! -f $ZDOTDIR/themes/.p10k-ascii.zsh ]] || source $ZDOTDIR/themes/.p10k-ascii.zsh
+fi
+prompt powerlevel10k
 
 # eval "$(oh-my-posh init zsh --config $XDG_CONFIG_HOME/omp/config.yml)"
 
