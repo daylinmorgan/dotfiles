f422790ad5773c33ccfe266863e942eeddcbf5f5
Daylin Morgan <me@dayl.in>
2024-10-17 12:37:49 -0500

    add fenfmt settings

--- home/private_dot_config/zsh/conf.d/env.zsh
+++ home/private_dot_config/zsh/conf.d/env.zsh
@@ -15,3 +15,7 @@ export MAGIC_ENTER_OTHER_COMMAND="ls -l ."
 # make less display nerd-font code points
 # export LESSUTFCHARDEF='E000-F8FF:p,F0000-FFFFD:p,100000-10FFFD:p'
 export LESSUTFCHARDEF='23fb-23fe:w,2665:w,2b58:w,e000-e00a:w,e0a0-e0a3:p,e0b0-e0bf:p,e0c0-e0c8:w,e0ca:w,e0cc-e0d7:w,e200-e2a9:w,e300-e3e3:w,e5fa-e6b5:w,e700-e7c5:w,ea60-ec1e:w,ed00-efce:w,f000-f2ff:w,f300-f375:w,f400-f533:w,f0001-f1af0:w'
+
+export FENFMT_PY_FORMATTER="ruff format"
+export FENFMT_PYTHON_FORMATTER="ruff format"
+
