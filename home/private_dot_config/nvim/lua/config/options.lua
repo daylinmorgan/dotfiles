703f1270f8fcb89f3f00671335247d1c9e083949
Daylin Morgan <me@dayl.in>
2024-10-20 12:44:48 -0500

    remove neovim cruft

--- home/private_dot_config/nvim/lua/config/options.lua
+++ home/private_dot_config/nvim/lua/config/options.lua
@@ -18,3 +18,5 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
 
 -- https://github.com/ziglang/zig.vim/issues/51#issuecomment-1637180522
 vim.g.zig_fmt_autosave = 0
+
+vim.lsp.set_log_level("off")
