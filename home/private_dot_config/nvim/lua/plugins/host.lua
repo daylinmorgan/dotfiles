a4d60021aa79d52cad1fea8e1895f0248b486118
Daylin Morgan <me@dayl.in>
2024-10-20 12:44:59 -0500

    add vim-be-good to play around

--- home/private_dot_config/nvim/lua/plugins/host.lua
+++ home/private_dot_config/nvim/lua/plugins/host.lua
@@ -3,26 +3,20 @@ local plugins = {}
 
 local single_host_plugins = {
   othalan = {
+    {'ThePrimeagen/vim-be-good'},  -- for fun + learning
     { "aklt/plantuml-syntax" },
     { "kaarmu/typst.vim" },
     {
       "mickael-menu/zk-nvim",
       config = function()
         require("zk").setup({
-          -- See Setup section below
         })
       end,
     },
-    { "Fymyte/rasi.vim" },
+    -- { "Fymyte/rasi.vim" },
     -- { dir = "~/dev/github/usu-dev/usu-vim/" },
   },
 }
-
-local function add_single_host_plugins()
-  if vim.tbl_contains(vim.tbl_keys(single_host_plugins), hostname) then
-    vim.list_extend(plugins, single_host_plugins[hostname])
-  end
-end
 
 if vim.tbl_contains({ "othalan", "algiz" }, hostname) then
   vim.list_extend(plugins, {
@@ -31,6 +25,8 @@ if vim.tbl_contains({ "othalan", "algiz" }, hostname) then
   })
 end
 
-add_single_host_plugins()
+if vim.tbl_contains(vim.tbl_keys(single_host_plugins), hostname) then
+  vim.list_extend(plugins, single_host_plugins[hostname])
+end
 
 return plugins
