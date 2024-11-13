ca75e319ef50bf17cd26b4ff245028a5e962b9da
Daylin Morgan <me@dayl.in>
2024-11-13 15:52:20 -0600

    typst on rentcomp

--- home/private_dot_config/nvim/lua/plugins/host.lua
+++ home/private_dot_config/nvim/lua/plugins/host.lua
@@ -5,7 +5,7 @@ local single_host_plugins = {
   othalan = {
     {'ThePrimeagen/vim-be-good'},  -- for fun + learning
     { "aklt/plantuml-syntax" },
-    { "kaarmu/typst.vim" },
+    -- { "kaarmu/typst.vim" },
     {
       "mickael-menu/zk-nvim",
       config = function()
@@ -17,6 +17,12 @@ local single_host_plugins = {
     -- { dir = "~/dev/github/usu-dev/usu-vim/" },
   },
 }
+
+if vim.tbl_contains({"othalan", "rentcomp01.ccbb.utexas.edu", "rentcomp02.ccbb.utexas.edu"}, hostname) then
+  vim.list_extend(plugins, {
+    {"kaarmu/typst.vim" },
+  })
+end
 
 if vim.tbl_contains({ "othalan", "algiz" }, hostname) then
   vim.list_extend(plugins, {
