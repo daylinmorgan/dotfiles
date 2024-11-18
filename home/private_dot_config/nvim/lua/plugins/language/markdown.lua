924582ab6b964378dce6ed2c1300ff5e2d3d1674
Daylin Morgan <me@dayl.in>
2024-11-18 11:02:01 -0600

    this plugin was giving me grief and I barely use it

--- home/private_dot_config/nvim/lua/plugins/language/markdown.lua
+++ home/private_dot_config/nvim/lua/plugins/language/markdown.lua
@@ -1,23 +1,23 @@
 return {
   require("util").setup_lang({ treesitter = { "markdown", "markdown_inline" } }),
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-    keys = {
-      {
-        "<leader>cp",
-        ft = "markdown",
-        "<cmd>MarkdownPreviewToggle<cr>",
-        desc = "Markdown Preview",
-      },
-    },
-    config = function()
-      vim.cmd([[do FileType]])
-    end,
-  },
+  -- {
+  --   "iamcco/markdown-preview.nvim",
+  --   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  --   build = function()
+  --     vim.fn["mkdp#util#install"]()
+  --   end,
+  --   keys = {
+  --     {
+  --       "<leader>cp",
+  --       ft = "markdown",
+  --       "<cmd>MarkdownPreviewToggle<cr>",
+  --       desc = "Markdown Preview",
+  --     },
+  --   },
+  --   config = function()
+  --     vim.cmd([[do FileType]])
+  --   end,
+  -- },
   {
     "MeanderingProgrammer/render-markdown.nvim",
     opts = {
