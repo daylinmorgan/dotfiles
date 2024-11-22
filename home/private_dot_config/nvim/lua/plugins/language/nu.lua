09e963987e1d97d1d69f500edb5aee04901a823f
Daylin Morgan <me@dayl.in>
2024-11-22 17:12:52 -0600

    update nushell neovim setup

--- home/private_dot_config/nvim/lua/plugins/language/nu.lua
+++ home/private_dot_config/nvim/lua/plugins/language/nu.lua
@@ -1,7 +1,18 @@
 return require("util").if_exe("nu", {
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = {
-    { "nushell/tree-sitter-nu" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "nu" }, -- Ensure the "nu" parser is installed
+        highlight = {
+          enable = true, -- Enable syntax highlighting
+        },
+      })
+    end,
+    dependencies = {
+      -- Additional Nushell parser
+      { "nushell/tree-sitter-nu", build = ":TSUpdate nu" },
+    },
+    build = ":TSUpdate",
   },
-  build = ":TSUpdate",
 })
