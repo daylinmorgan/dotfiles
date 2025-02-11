701402dfbaa6c792e50dbd0f93507fd6ba4787d8
Daylin Morgan <me@dayl.in>
2025-02-11 17:54:41 -0600

    work with roc again

--- home/private_dot_config/nvim/lua/plugins/language/roc.lua
+++ home/private_dot_config/nvim/lua/plugins/language/roc.lua
@@ -1,13 +1,4 @@
-return {
-  { "nvim-treesitter/nvim-treesitter", opts = { ensure_installed = { "roc" } } },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        roc_ls = {
-          mason = false,
-        },
-      },
-    },
-  },
-}
+return require("util").setup_lang({
+  treesitter = { "roc" },
+  lsp = { "roc_ls" },
+})
